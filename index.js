705f5c5b41e7f4d1b4b1dc78cc861da7485fedeb
DashiellLumas <dashiell.lumas@gmail.com>
2018-08-10 11:49:04 -0400

    installed node server

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+require('babel-register');
+
+const app = require('./server/server').app, PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log("application listening on port:", PORT);
+})
